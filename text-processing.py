36bdf8345ce8355fb82b784bbf144d859162f3e9
Narxene <59138233+DivaAnanda@users.noreply.github.com>
2025-03-23 23:45:11 +0800

    Update text-processing.py

--- text-processing.py
+++ text-processing.py
@@ -9,36 +9,36 @@ dokumen = [
     "Informasi tata cara daftar ulang bagi mahasiswa baru PTIIK kurang jelas. Sehingga ketika tanggal terakhir syarat penyerahan berkas daftar ulang, banyak mahasiswa baru yang tidak membawa salah satu syarat daftar ulangnya."
     ]
 
-#Langkah 1 - Tokenisasi
-#Pretokenisasi - Case Folding
+# Langkah 1 - Tokenisasi
+# Pretokenisasi - Case Folding
 dokumen = [d.lower() for d in dokumen]
 
-#Pretokenisasi - Menghilangkan angka, tanda baca dan karakter
+# Pretokenisasi - Menghilangkan angka, tanda baca dan karakter
 dokumen = [re.sub(r'[^a-z\s]', '', d) for d in dokumen]
 
-#Tokenisasi
+# Tokenisasi
 token = [d.split() for d in dokumen]
 print("\nHasil Tokenisasi:")
 for i, words in enumerate(token):
     print(f"Dokumen {i+1}: {words}")
 
-#Langkah 2 - Filtering (Stopword Removal)
+# Langkah 2 - Filtering (Stopword Removal)
 stopword_factory = StopWordRemoverFactory()
 stopwords = stopword_factory.get_stop_words()
-token = [[k for k in kal if k not in stopwords] for kal in token]
+token = [[word for word in sentence if word not in stopwords] for sentence in token]
 print("\nHasil Stopword Removal:")
 for i, words in enumerate(token):
     print(f"Dokumen {i+1}: {words}")
 
-#Langkah 3 - Stemming
+# Langkah 3 - Stemming
 stemmer_factory = StemmerFactory()
 stemmer = stemmer_factory.create_stemmer()
-token = [[stemmer.stem(word) for word in k] for k in token]
-print("\nHasil Tokenisasi:")
+token = [[stemmer.stem(word) for word in sentence] for sentence in token]
+print("\nHasil Stemming:")
 for i, words in enumerate(token):
     print(f"Dokumen {i+1}: {words}")
 
-#Langkah 4 - Hasil Preprocessing
+# Langkah 4 - Hasil Preprocessing
 print("\n--------------------------------------------------")
 print("Hasil Preprocessing:")
 for i, words in enumerate(token):
